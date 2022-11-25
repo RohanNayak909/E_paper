5e980e3424af8b62bc7eda8bbaa2899597c4cdb0
Priyanka Sahoo <priyanka.sahoo@nirmalyalabs.com>
2022-11-25 11:38:51 +0530

    issue-solve

--- src/app/component/admin/users/edituser/edituser.component.ts
+++ src/app/component/admin/users/edituser/edituser.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { MasterServiceService } from 'src/app/services/masterservice/master-service.service';
+import { UserService } from 'src/app/services/userservice/user.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -11,15 +12,18 @@ import { environment } from 'src/environments/environment';
 })
 export class EdituserComponent implements OnInit {
  
-  userData:any;
+  userData:any=[];
   dropdownSettings: IDropdownSettings = {};
   role:any;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private masterService: MasterServiceService) { }
+  allUser:any;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private masterService: MasterServiceService,
+    private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userData = this.data;
-    
-    console.log(this.userData)
+  //   this.userData = this.data;
+  // //  this.userData.user_status = this.data.status
+  // this.userData.role = this.userData.role.toString()
+  //   console.log(this.userData)
 
     this.dropdownSettings = {
       singleSelection: false,
@@ -30,6 +34,19 @@ export class EdituserComponent implements OnInit {
       itemsShowLimit: 1000,
       allowSearchFilter: true
     };
+    //this.getRoles();
+    this.getalluser();
+  }
+  getalluser() {
+    this.userService.getUserDetails('',this.data.user_id,environment.CUSTOMER_ID,'N').subscribe((data: any) => {
+      this.allUser = data.body
+      this.allUser = this.allUser.map((dt: any) => JSON.parse(dt));
+      this.userData = this.allUser[0]
+      this.userData.role = this.userData.role.toString()
+      this.userData.user_notification = this.userData.email_notify
+     
+      this.getRoles();
+    })
   }
   getRoles() {
     this.masterService.getRoles(environment.CUSTOMER_ID).subscribe((res: any) => {
