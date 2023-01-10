04fe712c95ad19793537aa3582f814140a061901
RohanNayak909 <75715641+RohanNayak909@users.noreply.github.com>
2023-01-10 15:20:49 +0530

    epaper

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  // POST_URL: 'https://prameyanews.com/post/',
+  POST_URL: 'https://prameyanews.com/post/',
   BASE_URL: 'https://api-dev.prameyanews.com/prameya/api',
+  PREVIEW_URL: 'https://api-dev.prameyanews.com/prameya/document/preview',
   CUSTOMER_ID : 3,
   CUSTOMER_NAME :'E-PAPER',
   PLATFORM_BASEURL: 'https://dev.prameyanews.com/',
