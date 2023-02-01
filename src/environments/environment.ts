058a36f99fdab8c629049c5549a6cc92ca26dfca
kirtee1996 <sagar.anchal45@gmail.com>
2023-02-01 15:00:11 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
   BASE_URL: 'https://moapi.prameyanews.com/prameya/api',
   CUSTOMER_ID : 3,
   CUSTOMER_NAME :'E-PAPER',
-  PLATFORM_BASEURL: 'https://dev.prameyanews.com/',
+  PLATFORM_BASEURL: 'https://prameyaepaper.com/',
   GOOGLE_ANALYTICS_ID: 'UA-139423842-3'
 };
